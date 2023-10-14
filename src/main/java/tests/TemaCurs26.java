d6501c5bede0c2e593fe062cde9da9e388eda960
Eliza Gutulov dorobantu <eliza.gutulov@yahoo.com>
2023-10-14 23:59:25 +0300

    Tema Curs 26 partea a doua

--- src/main/java/tests/TemaCurs26.java
+++ src/main/java/tests/TemaCurs26.java
@@ -3,8 +3,12 @@ package tests;
 import pages.MenuPage;
 import utils.BaseTest;
 
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -26,7 +30,17 @@ public class TemaCurs26 extends BaseTest{
 	public void verificare2() {
 		app.click(app.menu.shopLink);
 		app.click(app.shop.cookbooksCategory);	
-		////span[text()='Sale!']
+		List<WebElement> cartiAfisate = driver.findElements(By.xpath("//a[@class='hover_icon hover_icon_link']"));
+		cartiAfisate.size();
+		List<WebElement> cartiLaReducere = driver.findElements(By.xpath("//span[text()='Sale!']"));
+		//for (WebElement element : cartiAfisate) {
+		if(cartiAfisate.size()== cartiLaReducere.size()) {
+			System.out.println("Toate cartile sunt la reducere!");
+		}
+		else {
+			System.out.println("Nu toate cartile sunt la reducere!");
+		}
+		
 	}
 	
 	
--- src/main/java/tests/TemaCurs28.java
+++ src/main/java/tests/TemaCurs28.java
@@ -14,41 +14,62 @@ import utils.BaseTest;
 
 public class TemaCurs28 extends BaseTest{
 	
+//	@Test(priority=2)
+//	public void redirectFacebook1() throws InterruptedException {
+//		app.click(app.menu.homeLink);
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//		WebElement facebookLink = driver.findElement(app.menu.facebookLink);
+//		js.executeScript("arguments[0].click()", facebookLink);
+//		
+//		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+//		driver.switchTo().window(browserTabs.get(1));
+//		driver.findElement(By.xpath(" //div[ @tabindex='0'] //span[text()='Allow all cookies']")).click();
+//		assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/keytraining.ro");
+//		app.tearDown();
+//	}
+//
+//	@Test(priority=3)
+//	public void redirectTwitter() throws InterruptedException {
+//		app.setup("https://keybooks.ro/");
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//		WebElement twitterLink = driver.findElement(app.menu.twitterLink);
+//		js.executeScript("arguments[0].click()", twitterLink);
+//		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+//		driver.switchTo().window(browserTabs.get(1));
+//		assertEquals(driver.getCurrentUrl(), "https://twitter.com/");
+//		app.tearDown();
+//	}
+//	
+//	@Test(priority=4)
+//	public void redirectInstagram() throws InterruptedException {
+//		app.setup("https://keybooks.ro/");
+//		JavascriptExecutor js = (JavascriptExecutor) driver;
+//		WebElement instagramLink = driver.findElement(app.menu.instagramLink);
+//		js.executeScript("arguments[0].click()", instagramLink);
+//		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+//		driver.switchTo().window(browserTabs.get(1));
+//		assertEquals(driver.getCurrentUrl(), "https://www.instagram.com/");
+//		app.tearDown();
+//	}
+//	
 	@Test(priority=1)
-	public void redirectFacebook() throws InterruptedException {
-		app.click(app.menu.homeLink);
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement facebookLink = driver.findElement(app.menu.facebookLink);
-		js.executeScript("arguments[0].click()", facebookLink);
-		
-		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(browserTabs.get(1));
-		driver.findElement(By.xpath(" //div[ @tabindex='0'] //span[text()='Allow all cookies']")).click();
-		assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/keytraining.ro");
-		app.tearDown();
-	}
-
-	@Test(priority=2)
-	public void redirectTwitter() throws InterruptedException {
-		app.setup("https://keybooks.ro/");
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement twitterLink = driver.findElement(app.menu.twitterLink);
-		js.executeScript("arguments[0].click()", twitterLink);
-		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(browserTabs.get(1));
-		assertEquals(driver.getCurrentUrl(), "https://twitter.com/");
-		app.tearDown();
-	}
-	
-	@Test(priority=3)
-	public void redirectInstagram() throws InterruptedException {
-		app.setup("https://keybooks.ro/");
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		WebElement instagramLink = driver.findElement(app.menu.instagramLink);
-		js.executeScript("arguments[0].click()", instagramLink);
-		List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
-		driver.switchTo().window(browserTabs.get(1));
-		assertEquals(driver.getCurrentUrl(), "https://www.instagram.com/");
-		app.tearDown();
-	}
+    public void redirectFacebook() throws InterruptedException {
+          app.click(app.menu.homeLink);
+          WebElement facebookLink = driver.findElement(app.menu.facebookLink);
+          WebElement twitterLink = driver.findElement(app.menu.twitterLink);
+          WebElement instagramLink = driver.findElement(app.menu.instagramLink);  
+          WebElement[] socials =  {facebookLink, twitterLink, instagramLink};
+          String[] expectedURLs = {"https://www.facebook.com/keytraining.ro", 
+        		  "https://twitter.com/", "https://www.instagram.com/" };
+          for(int i=0; i<socials.length; i++) {
+               socials[i].click();
+               List <String> browserTabs = new ArrayList<String>(driver.getWindowHandles());
+               driver.switchTo().window(browserTabs.get(1));
+               assertEquals(driver.getCurrentUrl(), expectedURLs[i]);
+               driver.close();
+               driver.switchTo().window(browserTabs.get(0));
+          }
+    }
 }
+
+
